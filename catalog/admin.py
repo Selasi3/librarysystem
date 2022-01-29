fdf9f94d0c79bc58a65f34b802fd407dc6fd81fd
Selasi3 <selasiayittah3@gmail.com>
2022-01-29 06:13:50 -1200

    modified the AuthorAdmin class inthe admin.py file

--- catalog/admin.py
+++ catalog/admin.py
@@ -5,7 +5,8 @@ from .models import Author, Genre, Language, Book, BookInstance
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display=('last_name', "first_name", "date_of_birth", "date_of_death")
-    
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
