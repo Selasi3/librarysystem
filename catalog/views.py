3afd627fc0a3e70f4c28861864849fb7a7e9649c
Selasi3 <selasiayittah3@gmail.com>
2022-01-30 00:41:28 -1200

    modified the views.py

--- catalog/views.py
+++ catalog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre, Language
 # Create your views here.
 def index(request):
-    num_books = Book.objects.all()
+    num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()
