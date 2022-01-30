d5069d9f7bc15019bee7488149a75fd8922aa398
Selasi3 <selasiayittah3@gmail.com>
2022-01-30 00:57:54 -1200

    added the functionality to display total number of genres

--- catalog/views.py
+++ catalog/views.py
@@ -6,11 +6,13 @@ def index(request):
     num_instances = BookInstance.objects.all().count()
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()
+    num_genre = Genre.objects.all().count()
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_genre': num_genre,
     }
     return render(request, 'index.html', context=context)
 
