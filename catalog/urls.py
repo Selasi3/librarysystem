e14379d187240678181be181ea44728751bbb3a8
Selasi3 <selasiayittah3@gmail.com>
2022-02-05 04:04:00 -1200

    modified the models.py, book_list.html, book_detail.html

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
 
     path('', views.index, name="index"),
     path('books/', views.BookListView.as_view(), name='books'),
-    path('books/<int:pk>', views.BookListView.as_view(), name="book_detail"),
+    path('books/<int:pk>', views.BookDetailView.as_view(), name="book_detail"),
     path('authors/', views.AuthorListView.as_view(), name='authors'),
     path('authors/<int:pk>', views.AuthorListView.as_view(), name="author_detail"),
 
