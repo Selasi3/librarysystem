9aa40d354b0c8077adaba1d8c95538853a3fe1ec
Selasi3 <selasiayittah3@gmail.com>
2022-02-05 01:53:17 -1200

    created author_detail.html

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('books/', views.BookListView.as_view(), name='books'),
     path('books/<int:pk>', views.BookListView.as_view(), name="book_detail"),
-    path('authors/', views.AuthorListView.as_view(), name='authors')
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
+    path('authors/<int:pk>', views.AuthorListView.as_view(), name="author_detail"),
+
 ]
